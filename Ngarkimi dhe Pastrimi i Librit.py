2ab5af3bbca22d0bbd93fe6c2a1caa722d006d06
ArtAdemiUP <art.ademi@student.uni-pr.edu>
2026-04-18 14:55:14 +0200

    Pjesa e pare

---
+++ Ngarkimi dhe Pastrimi i Librit.py
@@ -0,0 +1,11 @@
+import random
+import string
+import os
+def ngarko_dhe_pastro_librin(emri_fajllit):
+    """Lexon librin dhe heq shenjat e pikësimit."""
+    if not os.path.exists(emri_fajllit):
+        with open(emri_fajllit, "w", encoding="utf-8") as f:
+            f.write("Siguria e te dhenave eshte nje lende shume interesante. "
+                    "Ne po mesojmë se si te kodojme mesazhe sekrete duke perdorur "
+                    "metoda te vjetra por efikase si kjo e Beale cipher.")
+        print(f"[!] U krijua nje skedar provizor '{emri_fajllit}' per testim.")
